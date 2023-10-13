bde8eef8d1f6c9620cdf1d722a46ff442387e104
Lavaret <czarnynozyk@gmail.com>
2023-10-13 16:52:03 +0200

    compass game

--- src/games/CompassGame.vue
+++ src/games/CompassGame.vue
@@ -1,22 +1,117 @@
 <template>
 
-  <div class="bg-cover bg-center block m-auto shadow rounded m-6 w-5/6 h-5/6"
+  <div class="bg-cover bg-center block m-auto shadow rounded relative m-6 w-5/6 h-5/6"
        :style="{'background-image':'url(/img/table.png)'}">
-    <div class="icon transition ease-in-out delay-150 hover:scale-110 duration-500 bg-gray-900 p-1 absolute m-2 rounded shadow flex gap-1">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-      </svg>
 
-      <p>Photo by <a href="https://unsplash.com/@thissillygirlskitchen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dana DeVolk</a> on <a href="https://unsplash.com/photos/JBqC2n_0zHM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+    <InfoCaption>
+      <p class="text-xs">Photo by
+        <a class="info-link " href="https://unsplash.com/@thissillygirlskitchen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dana DeVolk</a>
+        on
+        <a class="info-link" href="https://unsplash.com/photos/JBqC2n_0zHM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      </p>
+    </InfoCaption>
+
+    <div class="pt-20">
+      <div class="question bg-gray-900 p-1 text-lg m-auto rounded shadow">Where can you be if the compass works this way?</div>
     </div>
+
+    <img width="250" class="compass absolute shadow" :src="compassImage">
+    <img width="26" class="compass-arrow absolute" :src="compassArrowImage">
+
+    <div class="p-2 w-full absolute answer-input">
+      <div class="bg-gray-900 rounded shadow p-2">
+        <p :class="{'text-green-500' : answer !== '' && isValid === true, 'text-red-500' : answer !== '' && isValid === false}" class="text-left w-full">{{note}}</p>
+        <input @keydown.enter="checkAnswer" v-model="answer" class="bg-gray-800 h-8 w-full p-2 rounded shadow">
+      </div>
+    </div>
+
+
   </div>
 
 </template>
 
 <script setup>
-// import tableImage from "../img/table.png"
+
+import InfoCaption from "@/components/InfoCaption";
+import compassImage from "/public/img/compass.png";
+import compassArrowImage from '/public/img/compass-arrow.png';
+import { ref } from 'vue';
+
+let answer = ref('');
+let note = ref("Type your answer here:");
+let isValid = ref(null);
+
+function checkAnswer() {
+  let newAnswer = answer.value;
+  if(newAnswer.toLowerCase().replace(/\s/g, "").includes('pole')) {
+    note.value = "Great";
+    isValid.value = true;
+  } else {
+    note.value = "Try again";
+    isValid.value = false;
+  }
+}
+
+
 </script>
 
 <style scoped>
+.info-link {
+  @apply hover:underline underline-offset-2;
+}
 
+.answer-input {
+  top: 70vh;
+}
+
+.compass {
+  top: calc(33vh - 53px);
+  left: calc(50% - 125px);
+}
+
+.question {
+  max-width: 73%;
+}
+
+.compass-arrow {
+  top: calc(33vh - 71px + 125px);
+  left: calc(50% - 13px);
+  animation: compass-spin 5s ease-in infinite;
+}
+
+@keyframes compass-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  10% {
+    transform: rotate(80deg);
+  }
+  20% {
+    transform: rotate(60deg);
+  }
+  30% {
+    transform: rotate(100deg);
+  }
+  40% {
+    transform: rotate(120deg);
+  }
+  50% {
+    transform: rotate(100deg);
+  }
+  60% {
+    transform: rotate(180deg);
+  }
+  70% {
+    transform: rotate(200deg);
+  }
+  80% {
+    transform: rotate(240deg);
+  }
+  80% {
+    transform: rotate(220deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
