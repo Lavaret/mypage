7af2f3df93fda7fd187b6a7626aec31ef7e246e7
Lavaret <czarnynozyk@gmail.com>
2023-10-14 13:10:51 +0200

    intro

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
-<h1>Hi, I'm Justyna 👋</h1>
+  <div class="w-96 m-auto text-left">
+    <h1 class="text-lg">Hi, I'm Justyna 👋 </h1>
+    <p>and I'm a software developer.</p>
+    <p class="mt-2 text-sm">I'm not just passionate about writing code; I'm driven by the desire to create meaningful and impactful solutions that make a difference in the world. I'm excited to take on new challenges, learn new technologies, and contribute my expertise to innovative projects that drive progress and make a positive impact.</p>
+    <p class="mt-2 text-sm">I also like travelling, books and horror movies.</p>
+  </div>
 </template>
 
 <script setup>
