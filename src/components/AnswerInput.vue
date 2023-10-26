0ddb10e1c122d05551fa21fb30108e32626cda33
Lavaret <czarnynozyk@gmail.com>
2023-10-26 12:26:02 +0200

    change for computed property in class

--- src/components/AnswerInput.vue
+++ src/components/AnswerInput.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-gray-900 rounded shadow p-2">
-    <p :class="{'text-green-500' : isValid === true, 'text-red-500' : isValid === false}" class="text-left w-full mb-2">{{note}}</p>
+    <p :class="noteColor" class="text-left w-full mb-2">{{note}}</p>
     <input @keydown.enter="checkAnswer()" v-model="answer" class="bg-gray-800 h-8 w-full p-2 rounded shadow">
   </div>
 </template>
 
 <script setup>
-import { defineProps, ref, defineEmits } from 'vue';
+import { defineProps, ref, defineEmits, computed } from 'vue';
 import { useValidateAnswer } from "@/composables/useValidateAnswer";
 import {getRandomGood, getRandomWrong} from "@/composables/reactions";
 
@@ -14,12 +14,17 @@ let answer = ref('');
 const check = useValidateAnswer(answer, props.answers);
 let defaultNote = "Type your answer here: ";
 let note = ref(defaultNote);
-let isValid;
+let isValid = ref(null);
 
+const noteColor = computed(() => ({
+  'text-green-500' : isValid.value === true,
+  'text-red-500' : isValid.value === false
+}))
 const emit = defineEmits(['correctAnswer']);
 
 function checkAnswer() {
-  isValid = check();
+  let validation = check();
+  isValid.value = validation.value;
   if(isValid.value === false) {
     note.value = getRandomWrong();
   } else if(isValid.value === true) {
